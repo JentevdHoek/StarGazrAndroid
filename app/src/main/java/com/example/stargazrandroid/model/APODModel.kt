647db602577dee3adc30a63a583d4f386ad6d411
JentevdHoek <jentevanderhoek@gmail.com>
2024-03-28 10:21:09 +0100

    home screen

--- app/src/main/java/com/example/stargazrandroid/model/APODModel.kt
+++ app/src/main/java/com/example/stargazrandroid/model/APODModel.kt
@@ -1,14 +1,57 @@
 package com.example.stargazrandroid.model
-
-import com.google.gson.annotations.SerializedName
+import android.util.Log
+import java.net.HttpURLConnection
+import java.net.URL
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import org.json.JSONArray
+import org.json.JSONObject
+import java.io.FileNotFoundException
 
 data class APODModel(
-    @SerializedName("date") val date: String,
-    @SerializedName("explanation") var explanation: String,
-    @SerializedName("hdurl") val hdurl: String?,
-    @SerializedName("media_type") val mediaType: String,
-    @SerializedName("service_version") val serviceVersion: String,
-    @SerializedName("title") var title: String,
-    @SerializedName("url") val url: String,
-    @SerializedName("copyright") val copyright: String?
+    val date: String,
+    var explanation: String,
+    val hdurl: String?,
+    val media_type: String,
+    val service_version: String,
+    var title: String,
+    val url: String,
+    val copyright: String?
 )
+
+suspend fun fetchAPODData(date: String?): APODModel = withContext(Dispatchers.IO) {
+    val url = if (date != null) {
+        URL("https://api.nasa.gov/planetary/apod?api_key=ZoGElbDxAmYlXchoGyTsjep2MiAmptdcxrNg15vk&date=$date")
+    } else {
+        URL("https://api.nasa.gov/planetary/apod?api_key=ZoGElbDxAmYlXchoGyTsjep2MiAmptdcxrNg15vk&count=1")
+    }
+
+    val connection = url.openConnection()
+    val data: String
+    try {
+        data = connection.getInputStream().bufferedReader().use { it.readText() }
+    } catch (e: FileNotFoundException) {
+        Log.e("APODModel", "File not found: ${e.message}")
+        return@withContext APODModel("", "", "", "", "", "", "", "")
+    }
+
+    val jsonObject = if (data.trim().startsWith("[")) {
+        // Als data begint met "[", is het array, altijd maar 1 object in array
+        val jsonArray = JSONArray(data)
+        jsonArray.getJSONObject(0)
+    } else {
+        // anders object
+        JSONObject(data)
+    }
+
+    APODModel(
+        date = jsonObject.getString("date"),
+        explanation = jsonObject.getString("explanation"),
+        hdurl = jsonObject.getString("hdurl"),
+        media_type = jsonObject.getString("media_type"),
+        service_version = jsonObject.getString("service_version"),
+        title = jsonObject.getString("title"),
+        url = jsonObject.getString("url"),
+        copyright = jsonObject.optString("copyright")
+    )
+}
